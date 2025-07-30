b6b1afb053a1e862260450b93aec6973454ef0ed
Pieter Naber <github.com@mantix.nl>
2025-07-30 12:20:57 +0200

    Refactor card component styles and structure: updated padding classes, improved background image handling, and simplified title and footer checks for better readability.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,14 +1,14 @@
 @props(['title' => null, 'square' => false, 'background_image' => null, 'background_color' => null, 'text_color' => null, 'footer' => null])
 
-<div {{ $attributes->merge(['class' => 'card ' . ($square ? 'card-square' : '') . ' my-3']) }} style="{{ isset($background_image) ? 'background-image: url(\'' . $background_image . '\'); background-size: cover; background-position: center center;' : '' }}">
-    @if (isset($title) && strval($title))
-        <h4 class="card-header {{ isset($background_color) ? 'bg-' . $background_color : '' }} {{ isset($text_color) ? 'text-' . $text_color : '' }} p-4 pb-3">{!! $title !!}</h4>
+<div {{ $attributes->merge(['class' => 'card ' . ($square ? 'card-square' : '') . ' mb-3']) }} style="{{ !empty($background_image) ? 'background-image: url(\'' . $background_image . '\'); background-size: cover; background-position: center center;' : '' }}">
+    @if (!empty($title))
+        <h4 class="card-header {{ !empty($background_color) ? 'bg-' . $background_color : '' }} {{ !empty($text_color) ? 'text-' . $text_color : '' }} p-3">{!! $title !!}</h4>
     @endif
-    <div class="card-body p-4">
+    <div class="card-body p-3">
         {{ $slot }}
     </div>
-    @if (isset($footer) && strval($footer))
-        <div class="card-footer p-4">
+    @if (!empty($footer))
+        <div class="card-footer p-3">
             {{ $footer }}
         </div>
     @endif
