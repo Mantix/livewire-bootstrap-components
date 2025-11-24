3fc7ceb428a67d0caaaa73d98fd2f422e1320b83
Pieter Naber <github.com@mantix.nl>
2025-11-24 15:50:04 +0100

    Add body_class prop to card component for enhanced customization. This allows for additional styling options in the card body, improving flexibility in design.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,10 +1,10 @@
-@props(['title' => null, 'background_image' => null, 'background_color' => null, 'text_color' => null, 'footer' => null])
+@props(['title' => null, 'background_image' => null, 'background_color' => null, 'text_color' => null, 'footer' => null, 'body_class' => null])
 
 <div {{ $attributes->class(['card', 'mb-3' => strpos($attributes->get('class'), 'mb-') === false]) }} style="{{ !empty($background_image) ? 'background-image: url(\'' . $background_image . '\'); background-size: cover; background-position: center center;' : '' }}">
     @if (!empty($title))
         <h4 class="card-header {{ !empty($background_color) ? 'bg-' . $background_color : '' }} {{ !empty($text_color) ? 'text-' . $text_color : '' }} p-3">{!! $title !!}</h4>
     @endif
-    <div {{ $attributes->class(['card-body', 'p-3' => strpos($attributes->get('class'), 'p-') === false]) }}>
+    <div class="card-body {{ !empty($body_class) && strpos($body_class, 'p-') === false ? '' : 'p-3' }} {{ $body_class ?? '' }}">
         {{ $slot }}
     </div>
     @if (!empty($footer))
