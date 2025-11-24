4ec95705bbaf5df80c6e749839d497aaee8bfd37
Pieter Naber <github.com@mantix.nl>
2025-11-24 14:58:44 +0100

    Update card component class to improve consistency in styling. Changed class from 'card-body' to 'card' for better alignment with Bootstrap standards.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 @props(['title' => null, 'background_image' => null, 'background_color' => null, 'text_color' => null, 'footer' => null])
 
-<div {{ $attributes->class(['card-body', 'mb-3' => strpos($attributes->get('class'), 'mb-') === false]) }} style="{{ !empty($background_image) ? 'background-image: url(\'' . $background_image . '\'); background-size: cover; background-position: center center;' : '' }}">
+<div {{ $attributes->class(['card', 'mb-3' => strpos($attributes->get('class'), 'mb-') === false]) }} style="{{ !empty($background_image) ? 'background-image: url(\'' . $background_image . '\'); background-size: cover; background-position: center center;' : '' }}">
     @if (!empty($title))
         <h4 class="card-header {{ !empty($background_color) ? 'bg-' . $background_color : '' }} {{ !empty($text_color) ? 'text-' . $text_color : '' }} p-3">{!! $title !!}</h4>
     @endif
