b236a8242913d479d374243d156efec3b920c4e2
Pieter Naber <github.com@mantix.nl>
2025-12-11 09:40:57 +0100

    Enhance card component to support actions prop and improve header layout. Added conditional rendering for title and actions, allowing for better customization and alignment in the card header.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,14 @@
-@props(['title' => null, 'background_image' => null, 'background_color' => null, 'text_color' => null, 'footer' => null, 'body_class' => null])
+@props(['title' => null, 'background_image' => null, 'background_color' => null, 'text_color' => null, 'footer' => null, 'body_class' => null, 'actions' => []])
 
 <div {{ $attributes->class(['card', 'mb-3' => strpos($attributes->get('class'), 'mb-') === false]) }} style="{{ !empty($background_image) ? 'background-image: url(\'' . $background_image . '\'); background-size: cover; background-position: center center;' : '' }}">
-    @if (!empty($title))
-        <h4 class="card-header {{ !empty($background_color) ? 'bg-' . $background_color : '' }} {{ !empty($text_color) ? 'text-' . $text_color : '' }} p-3">{!! $title !!}</h4>
+    @if (!empty($title) || !empty($actions))
+        <div class="card-header d-flex justify-content-between align-items-center p-3">
+            @if (!empty($title))
+                <h4 class="{{ !empty($background_color) ? 'bg-' . $background_color : '' }} {{ !empty($text_color) ? 'text-' . $text_color : '' }}">{!! $title !!}</h4>
+            @endif
+            @if (!empty($actions))
+            @endif
+        </div>
     @endif
     <div class="card-body {{ !empty($body_class) && strpos($body_class, 'p-') === false ? 'p-3' : '' }} {{ $body_class ?? '' }}">
         {{ $slot }}
