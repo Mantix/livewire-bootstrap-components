573d9315dca0712f070528bd2b98f26ef582b6ac
Pieter Naber <github.com@mantix.nl>
2025-12-15 15:13:06 +0100

    Enhance card component header to conditionally apply background and text colors based on props. This improves customization options for card headers, aligning with design requirements.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -2,9 +2,9 @@
 
 <div {{ $attributes->class(['card', 'mb-3' => strpos($attributes->get('class'), 'mb-') === false]) }} style="{{ !empty($background_image) ? 'background-image: url(\'' . $background_image . '\'); background-size: cover; background-position: center center;' : '' }}">
     @if (!empty($title) || !empty($actions))
-        <div class="card-header d-flex justify-content-between align-items-center p-3">
+        <div class="card-header d-flex justify-content-between align-items-center p-3 {{ !empty($background_color) ? 'bg-' . $background_color : '' }} {{ !empty($text_color) ? 'text-' . $text_color : '' }}">
             @if (!empty($title))
-                <h4 class="{{ !empty($background_color) ? 'bg-' . $background_color : '' }} {{ !empty($text_color) ? 'text-' . $text_color : '' }}">{!! $title !!}</h4>
+                <h4>{!! $title !!}</h4>
             @endif
             @if (!empty($actions))
                 <x-bootstrap::actions :actions="$actions" class="ms-2" />
